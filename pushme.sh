c7f42e0482bf65be7d41e0706cda8d3dc4f689a3
Jorge Nogales <jorge.nogales@gmail.com>
2025-12-05 13:21:40 +0100

    some changes

--- pushme.sh
+++ pushme.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gemini --model "gemini-2.5-flash" --yolo "Use git to commit and push the current branch changes into the repo" > /dev/null
+gemini --model "gemini-2.5-flash" --yolo "Use git to commit and push the current changes into the repo" > /dev/null
